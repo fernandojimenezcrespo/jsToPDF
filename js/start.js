a78ddc09e677b4cb65832260a62f58c348b3f12b
fernandojimenezcrespo <fernandojimenezcrespo@gmail.com>
2022-08-16 14:09:15 +0200

    modificaciones titulo

--- js/start.js
+++ js/start.js
@@ -20,7 +20,7 @@ function generaAutoTableHECES()
       doc.autoTable({
     //startY: finalY + 20,
           theme: 'grid',
-    head: [['Ord.','HISTORIA', 'NOMBRE', 'SOLICITADO', 'RESULTADOS', 'OBSERVACION']],
+    head: [['Ord.','HISTORIA', 'NOMBRE                                                           ', 'SOLICITADO', 'RESULTADOS', 'OBSERVACION']],
     body: [
         ['1','', '', '', '', ''],
         ['2','', '', '', '', ''],
@@ -52,15 +52,17 @@ doc.save('ControlHeces.pdf')
 function generaAutoTableSENSOR()
 {
     var doc = new jsPDF('l')
+    doc.text(20,10,' CONTROL DE SENSORES DEL AÑO 2022');
   doc.autoTable({
     html: '.table',
     tableWidth: 'wrap',
     styles: { cellPadding: 0.5, fontSize: 8 },
+      
   })
       doc.autoTable({
     //startY: finalY + 20,
           theme: 'grid',
-    head: [['Ord.','PACIENTE', 'ENE-2023', 'FEB-2023', 'MAR-2023', 'ABR-2023', 'MAY-2023', 'JUN-2023', 'JUL-2023', 'AGO-2023', 'SEP-2023', 'OCT-2023', 'NOV-2023', 'DIC-2023']],
+    head: [['Ord.','PACIENTE', 'ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC']],
     body: [
         ['1', 'ALEX MAYORGA ', '', '', '', '', '', '', '', '', '', '', '', ''],
         ['2', 'CRISTINA JIMENEZ', '', '', '', '', '', '', '', '', '', '', ''],
